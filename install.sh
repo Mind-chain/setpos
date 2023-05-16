7d6c7844c9ba5fd6dbd29a3d9ebacc375893b981
root <root@testnet.web3twenty.com>
2023-05-16 04:48:16 +0000

    Gen upd

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-https://github.com/nnlgsakib/MIND-pos
+git clone https://github.com/nnlgsakib/MIND-pos
 cd MIND-pos
 go build -o mind main.go
 sudo mv mind /usr/local/bin
 cd ./../
-rm -rf mind-pos
+rm -rf MIND-pos
 mind version
