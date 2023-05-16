ced757c67296267d281ebc5fef0e498dda6d2805
root <root@testnet.web3twenty.com>
2023-05-16 16:55:40 +0000

    added centos firewall

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+sudo firewall-cmd --zone=public --add-port=8545/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=10001/tcp --permanent
 ufw allow 10001
 ufw allow  8545
 mind server --data-dir=node --chain genesis.json --libp2p 0.0.0.0:10001 --nat 0.0.0.0 --jsonrpc 0.0.0.0:8545 --seal 
