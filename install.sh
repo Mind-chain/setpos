d7a964f75a1ee75f879f86a3a7d303ba80519e84
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2023-06-05 17:55:46 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-git clone https://github.com/MetaViralBit/MetaViral-Blockchain
-cd MetaViral-Blockchain
+git clone https://github.com/nnlgsakib/mindpos.l2
+cd mindpos.l2
 go build -o mind main.go
 sudo mv mind /usr/local/bin
 cd ./../
-rm -rf MetaViral-Blockchain
+rm -rf mindpos.l2
 mind version
