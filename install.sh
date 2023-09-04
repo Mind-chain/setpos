994ec20e8a228e77af8c630558db2fb6b5e0a5ae
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2023-09-04 23:24:19 +0600

    Added MIND v2

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-git clone https://github.com/Mind-chain/MIND-smart-chain
-cd MIND-smart-chain
+git clone https://github.com/Mind-chain/Msc-node
+cd Msc-node
 go build -o mind main.go
 sudo mv mind /usr/local/bin
 cd ./../
-rm -rf MIND-smart-chain
+rm -rf Msc-node
 mind version
