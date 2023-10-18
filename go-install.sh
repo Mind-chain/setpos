9e7d61b1339376e9a2564dba4324eae9fbba3ad1
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2023-10-18 13:41:12 +0600

    Update go-install.sh

--- go-install.sh
+++ go-install.sh
@@ -1,8 +1,12 @@
+cd 
+
 wget https://golang.org/dl/go1.20.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 source  ~/.bashrc
 
 go version
+
+cd setpos 
 
 
