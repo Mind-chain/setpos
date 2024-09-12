b8088f5bba7c0ff8eb1f724420b7c71795a4e0ce
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-09-12 17:44:57 +0600

    Update go-install.sh

--- go-install.sh
+++ go-install.sh
@@ -1,42 +1,21 @@
-
 #!/bin/bash
 
-# Function to display usage
-usage() {
-    echo "Usage: $0 --v <version>"
+# Check if GO_VERSION is set, if not, use a default or exit
+if [ -z "$GO_VERSION" ]; then
+    echo "Error: Go version not specified. Please set the GO_VERSION environment variable."
     exit 1
-}
-
-# Parse input arguments
-while [[ "$1" != "" ]]; do
-    case $1 in
-        --v )
-            shift
-            VERSION=$1
-            ;;
-        * )
-            usage
-            ;;
-    esac
-    shift
-done
-
-# Check if version is specified
-if [ -z "$VERSION" ]; then
-    echo "Error: Go version not specified."
-    usage
 fi
 
 # Define the Go download URL
-GO_URL="https://golang.org/dl/go${VERSION}.linux-amd64.tar.gz"
+GO_URL="https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz"
 
 # Download the Go tarball
-echo "Downloading Go version $VERSION..."
-wget $GO_URL -O go${VERSION}.linux-amd64.tar.gz
+echo "Downloading Go version $GO_VERSION..."
+wget $GO_URL -O go${GO_VERSION}.linux-amd64.tar.gz
 
 # Install Go
 echo "Installing Go..."
-sudo tar -C /usr/local -xzf go${VERSION}.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
 
 # Add Go to the PATH
 echo "Adding Go to PATH..."
@@ -48,8 +27,3 @@ source ~/.bashrc
 # Verify installation
 echo "Go installed successfully. Version:"
 go version
-
-
-cd setpos 
-
-
