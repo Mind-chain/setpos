dbda2965e93e3b49b9fcd70f8023d4d9d1cbab1e
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-09-12 17:43:27 +0600

    Update go-install.sh

--- go-install.sh
+++ go-install.sh
@@ -1,11 +1,54 @@
-cd 
 
-wget https://golang.org/dl/go1.20.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
+#!/bin/bash
+
+# Function to display usage
+usage() {
+    echo "Usage: $0 --v <version>"
+    exit 1
+}
+
+# Parse input arguments
+while [[ "$1" != "" ]]; do
+    case $1 in
+        --v )
+            shift
+            VERSION=$1
+            ;;
+        * )
+            usage
+            ;;
+    esac
+    shift
+done
+
+# Check if version is specified
+if [ -z "$VERSION" ]; then
+    echo "Error: Go version not specified."
+    usage
+fi
+
+# Define the Go download URL
+GO_URL="https://golang.org/dl/go${VERSION}.linux-amd64.tar.gz"
+
+# Download the Go tarball
+echo "Downloading Go version $VERSION..."
+wget $GO_URL -O go${VERSION}.linux-amd64.tar.gz
+
+# Install Go
+echo "Installing Go..."
+sudo tar -C /usr/local -xzf go${VERSION}.linux-amd64.tar.gz
+
+# Add Go to the PATH
+echo "Adding Go to PATH..."
 export PATH=$PATH:/usr/local/go/bin
-source  ~/.bashrc
 
+# Source .bashrc to apply changes
+source ~/.bashrc
+
+# Verify installation
+echo "Go installed successfully. Version:"
 go version
+
 
 cd setpos 
 
